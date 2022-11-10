2b5c70928738626740da386f6cad610cc2a2e434
Valentin Boutrouche <vboutrou@mathworks.com>
2022-11-10 15:51:29 -0500

    Refactor ManOnTheMoon, add text and feedback

--- InternalFiles/MCCGrader.m
+++ InternalFiles/MCCGrader.m
@@ -1,77 +1,70 @@
 classdef MCCGrader
 
-    properties (Access = private)
-        m_numquery (1,1) int16 {mustBeGreaterThanOrEqual(m_numquery, 0)} = 0
-        m_numright (1,1) int16 {mustBeGreaterThanOrEqual(m_numright, 0)} = 0
-        m_name     (1,1) string = "NaN - NotAName"
-    end
-    
     methods
 
-        function obj = MCCGrader(varargin)
-            
-            if nargin == 1
-                in = varargin{1};
-                if isstring(in)
-                    obj.m_name = obj.RegisterName(in);
-                elseif islogical(in)
-                    [obj.m_numright, obj.m_numquery] = obj.RegisterAnswer(in);
-                end
-            end
+        function obj = MCCGrader(opts)
 
-        end
-
-        function out = Report(obj, feedback)
             arguments
-                obj (1,1) MCCGrader
-                feedback (1,1) string = ""
-            end
-            m_name = obj.RegisterName
-%             out = table(self.m_name, self.m_numright, self.m_numquery, feedback, ...
-%                 'VariableNames',{'Name', 'Score', 'Questions', 'Feedback'});
-        end
-
-        function Reset(self)
-            clear self.RegisterAnswer()
-            clear self.RegisterName()
-        end
-    end
-
-    methods (Static)
-
-        function [r, n] = RegisterAnswer(cmp)
-            arguments 
-                cmp (1,1) logical = false
+                opts.Name     (1,1) string  = ""
+                opts.Feedback (1,1) string  = ""
+                opts.Reset    (1,1) logical = false
+                opts.Code     (1,1) uint8   = 0
             end
 
-            % Declare the persistent variable
-            persistent numquery;
-            persistent numright;
-            if isempty(numquery)
-                numquery = 0;
+            persistent name;
+            persistent feedback;
+            persistent isinit;
+            if isempty(name)
+                name = "";                
             end
-            if isempty(numright)
-                numright = 0;
+            if isempty(feedback)
+                feedback = "";
+            end
+            if isempty(isinit)
+                isinit = false;
+            end
+
+            if ( opts.Code ~= 0 )
+                RegisterEntry = true;
+            else
+                RegisterEntry = false;
             end
             
-            % Increment the counter
-            numquery = numquery+ 1;
-            if cmp
-                numright = numright + 1;
+            if ( opts.Name ~= "" )
+                name = opts.Name;
+                isinit = true;
+                RegisterEntry = false;
             end
-            r = numright;
-            n = numquery;
-        end
-
-        function n = RegisterName(name)
-
-            persistent student;
-            if isempty(student)
-                student = name;
+            if ( opts.Feedback ~= "" )
+                feedback = opts.Feedback;
+                RegisterEntry = false;
             end
-            disp(student)
-            n = student;
+            if ( opts.Reset )
+                name = [];
+                feedback = [];
+                RegisterEntry = false;
+            end
+            
+            [~,File,Ext] = fileparts(matlab.desktop.editor.getActiveFilename);
+            if ( RegisterEntry )
+                Stack = dbstack();
+                Timestamp = datetime;
+                if size(Stack, 1) > 1
+                    Location = string(Stack(2).name);
+                else 
+                    Location = string(Stack.name);
+                end
+                Code = opts.Code;
+                Release = matlabRelease.Release;
+                Platform = string(computer);
+                
+                
+                Entry = table(Timestamp, Location, Code, Release, Platform)
+            end
+
+
         end
 
     end
+
 end
