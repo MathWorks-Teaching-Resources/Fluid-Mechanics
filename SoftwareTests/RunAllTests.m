10ca1aad003602bd67da40daf4abfa5c0163897d
Valentin Boutrouche <vboutrou@mathworks.com>
2023-11-14 08:05:30 -0500

    Modify CI

---
+++ SoftwareTests/RunAllTests.m
@@ -0,0 +1,16 @@
+% Create the test suite with SmokeTest and Function test if they exist
+Suite = testsuite("SmokeTests");
+Suite = [Suite;testsuite("FunctionTests")];
+
+% Create a runner with no plugins
+Runner = matlab.unittest.TestRunner.withNoPlugins;
+
+% Run the test suite
+Results = Runner.run(Suite);
+
+% Format the results in a table and save them
+ResultsTable = table(Results');
+writetable(ResultsTable,fullfile("SoftwareTests","TestResults_"+release_version+".txt"));
+
+% Assert success of test
+assertSuccess(Results)
--- SoftwareTests/SmokeTests.m
+++ SoftwareTests/SmokeTests.m
@@ -1,74 +1,42 @@
-% Run these tests with runMyTests
-% All tests so far are on code expected to run without errors
-% If/when we end up with a version that _should_ error, 
-% please add it to this set of examples
 classdef SmokeTests < matlab.unittest.TestCase
     
-    properties
-        rootProject
-        results
+    properties (ClassSetupParameter)
+        Project = {''};
     end
 
+    properties (TestParameter)
+        Scripts;
+    end
 
-     methods (TestClassSetup)
+    methods (TestParameterDefinition,Static)
 
-        function setUpPath(testCase)
-            
+        function Scripts = GetScriptName(Project)
+            RootFolder = currentProject().RootFolder;
+            Scripts = dir(fullfile(RootFolder,"Scripts","*.mlx"));
+            Scripts = {Scripts.name};
+        end
+
+    end
+
+    methods (TestClassSetup)
+
+        function SetUpSmokeTest(testCase,Project)
             try
-                project = currentProject;
-                testCase.rootProject = project.RootFolder;
-                cd(testCase.rootProject)
+               currentProject;   
             catch
-                error("Load project prior to run tests")
+                error("Project is not loaded.")
             end
-            
-            testCase.log("Running in " + version)
+        end
 
-        end % function setUpPath
-
-        function setUpResults(testCase)
-            files = dir(fullfile(testCase.rootProject,"Scripts","*.mlx"));
-            testCase.results = struct;
-            testCase.results.Name = strings(size(files));
-            testCase.results.Passed = false(size(files));
-            testCase.results.Time = zeros(size(files));
-            testCase.results.Message = strings(size(files));
-            for k = 1:length(files)
-                testCase.results.Name(k) = string(files(k).name);
-            end
-
-        end % function setUpResults
-
-    end % methods (TestClassSetup)
+    end
 
     methods(Test)
 
-        function smokeTest(testCase)
-            myFiles = testCase.results.Name;
-            fid = fopen(fullfile("SoftwareTests","TestResults_"+release_version+".txt"),"w");
-            fprintf(fid,"Version,File,Status,ElapsedTime\n");
-            for kTest = 1:length(myFiles)
-                try
-                    disp("Running " + myFiles(kTest))
-                    tic
-                    run(myFiles(kTest))
-                    testCase.results.Time(kTest) = toc;
-                    disp("Finished " + myFiles(kTest))
-                    testCase.results.Passed(kTest) = true;
-                    fprintf(fid,"%s,%s,%s,%s\n",release_version,myFiles(kTest),"passed",testCase.results.Time(kTest));
-                catch ME
-                    testCase.results.Time(kTest) = toc;
-                    disp("Failed " + myFiles(kTest) + " because " + ...
-                        newline + ME.message)
-                    testCase.results.Message(kTest) = ME.message;
-                    fprintf(fid,"%s,%s,%s,%s\n",release_version,myFiles(kTest),"failed",testCase.results.Time(kTest));
-                end
-                clearvars -except kTest testCase myFiles fid
-            end
-            fclose(fid);
-            struct2table(testCase.results)
+        function SmokeRun(testCase,Scripts)
+            disp(">> Running "+ Scripts)
+            evalc("run "+ Scripts);
         end
-
+            
     end
 
     methods (TestClassTeardown)
@@ -78,6 +46,6 @@ classdef SmokeTests < matlab.unittest.TestCase
             bdclose all      % Close Simulink models
         end
 
-    end % methods (TestClassTeardown)
+    end
 
 end
